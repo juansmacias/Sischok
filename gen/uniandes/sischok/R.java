f3545668f08bb1021acffee112c6afd4bcf68ec4
lainis20 <lainis20@gmail.com>
2014-09-13 20:11:07 -0500

    Icono de sischock

--- gen/uniandes/sischok/R.java
+++ gen/uniandes/sischok/R.java
@@ -34,12 +34,13 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
-        public static final int icono7=0x7f020001;
-        public static final int slidingmenu_foto_perfil=0x7f020002;
-        public static final int slidingmenu_icono=0x7f020003;
-        public static final int slidingmenu_list_selector_background=0x7f020004;
-        public static final int slidingmenu_shadow=0x7f020005;
-        public static final int url=0x7f020006;
+        public static final int icono=0x7f020001;
+        public static final int icono7=0x7f020002;
+        public static final int slidingmenu_foto_perfil=0x7f020003;
+        public static final int slidingmenu_icono=0x7f020004;
+        public static final int slidingmenu_list_selector_background=0x7f020005;
+        public static final int slidingmenu_shadow=0x7f020006;
+        public static final int url=0x7f020007;
     }
     public static final class id {
         public static final int incidente_detail=0x7f080002;
