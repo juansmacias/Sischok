2d2008945381d11433c41d1a52dbd82ac09f8b0e
lainis20 <lainis20@gmail.com>
2014-09-15 19:13:47 -0500

    muchos cambios para que funcione

--- src/uniandes/sischok/mundo/CentroEventos.java
+++ src/uniandes/sischok/mundo/CentroEventos.java
@@ -9,14 +9,20 @@ import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.provider.CalendarContract;
+import android.provider.CalendarContract.Calendars;
+import android.provider.CalendarContract.Events;
 import android.widget.Toast;
 
 public class CentroEventos extends IntentService
 {
+	
+	public static Context context;
 		
 	public CentroEventos() 
 	{
@@ -37,58 +43,54 @@ public class CentroEventos extends IntentService
 	@Override
 	protected void onHandleIntent(Intent intent) 
 	{
+		CentroEventos.context = getApplicationContext();
+		
 		// TODO Auto-generated method stub lo que hace cuando el servicio start
 		//run query
 		Cursor cur = null;
-		ContentResolver cr = getContentResolver();
-		String uriString = "content://com.android.calendar/events"; 
-		Uri uri = Uri.parse(uriString); 
+		ContentResolver cr = context.getContentResolver();
 		
-		String currentDate = new SimpleDateFormat("yyyy-MM-dd", java.util.Locale.getDefault()).format(new Date());
 		
-		String selection = "((" + CalendarContract.Events.DTSTART + "='"+ currentDate+"'))";
-		System.out.println(selection);
-		String[] selectionArgs = new String[] {currentDate}; 
-//		Uri uri = CalendarContract.Events.CONTENT_URI; version >14
-		cur = cr.query(uri, new String[]{"_id", "title", "description", "dtstart", "dtend", "eventLocation"}, selection, null, null);
+//    	Toast.makeText(getApplicationContext(), "Revisar Sichock: la prueba de las nots", Toast.LENGTH_LONG).show();
+		
+    	Long timeNow = new Date().getTime();
+		Uri.Builder eventsUriBuilder = CalendarContract.Instances.CONTENT_URI.buildUpon();
+		ContentUris.appendId(eventsUriBuilder, timeNow);
+		ContentUris.appendId(eventsUriBuilder, (timeNow +(60000 * 120)));
+		Uri eventsUri = eventsUriBuilder.build();
+		cur = cr.query(eventsUri, new String[]{"_id", "title", "description", "dtstart", "dtend", "eventLocation"}, null, null, CalendarContract.Instances.DTSTART + " ASC");
+		
 		cur.moveToFirst();
+		
 		//arreglo de los eventos de cada dia
         String[] CalTitle = new String[cur.getCount()];
-        System.out.println("Cuantos coge = " +CalTitle.length);
-        // recorre los eventos del dia para notificar unicamente los que van a suceder en la proxima hora y 59 min
+//    	Toast.makeText(getApplicationContext(), "Eventos: "+CalTitle.length, Toast.LENGTH_LONG).show();
+    	//yayyy si saleeeeee
+    	
+        // recorre los eventos resultados de la consulta para notificar unicamente los que van a suceder en la proxima hora y 59 min
         for (int i = 0; i < CalTitle.length; i++) 
         {
             CalTitle[i] = cur.getString(1);
-            Date sDate = new Date(cur.getLong(3));
-            //6000 milisegundos en un minuto
-            Date limitDate = new Date(cur.getLong(3)+(119*60000));
-            if(sDate.after(limitDate))
-            { 
-            	//Notifica
-//            	NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+            
+            	NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
             	// prepare intent which is triggered if the notification is selected
 //            	intent = new Intent(this, NotificationReceiver.class);
-//            	PendingIntent pIntent = PendingIntent.getActivity(this, 0, intent, 0);
-            	
-//            	Notification.Builder builder  = new Notification.Builder(this) A
-//            	Notification n  = new Notification.Builder(this)
-//                .setContentTitle("Revisar zonas peligrosas ")
-//                .setContentText("Evento: "+ cur.getString(3))
-//                .setSmallIcon(R.drawable.icono)
-//                .setContentIntent(pIntent)
-//                .setAutoCancel(true).build();
+            	PendingIntent pIntent = PendingIntent.getActivity(this, 0, intent, 0);
+//            	Notification.Builder builder  = new Notification.Builder(this);
+            	Notification n  = new Notification.Builder(this)
+                .setContentTitle("Revisar zonas peligrosas ")
+                .setContentText("Evento: "+ cur.getString(1))
+                .setSmallIcon(R.drawable.icono)
+                .setContentIntent(pIntent)
+                .setAutoCancel(true).build();
 //           NO     .addAction(R.drawable.icon, "Call", pIntent)
 //             NO   .addAction(R.drawable.icon, "More", pIntent)
 //               NO .addAction(R.drawable.icon, "And more", pIntent).build();
-            	
 //            		Notification n = builder.build();A
-            	
-//            	notificationManager.notify(0, n);
+            	notificationManager.notify(0, n);
             	//easy way
             	Toast.makeText(getApplicationContext(), 
                         "Revisar Sichock: "+CalTitle[i], Toast.LENGTH_LONG).show();
-
-            } 
             cur.moveToNext();
         }
         stopSelf();
