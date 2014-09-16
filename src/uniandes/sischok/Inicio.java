ea27e3460acaf8deb404fcc71c01a610ae08b99d
juansmacias <juansmacias@gmail.com>
2014-09-16 00:37:03 -0500

    oli

--- src/uniandes/sischok/Inicio.java
+++ src/uniandes/sischok/Inicio.java
@@ -18,9 +18,19 @@ import org.json.JSONObject;
 import de.greenrobot.dao.query.QueryBuilder;
 
 import uniandes.sischok.mundo.DaoMaster.DevOpenHelper;
+import java.util.Calendar;
+
+import java.util.Calendar;
+
+import java.util.Calendar;
+
+import java.util.Calendar;
+
 import android.os.Bundle;
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -119,6 +129,21 @@ public class Inicio extends Activity {
 		    });
 		Intent intent = new Intent(this, CentroEventos.class);
 		startService(intent);
+		
+		// TODO: If exposing deep links into your app, handle intents here.
+		//crea el intent de las notificaciones
+		
+		 // Start service Centro Eventos using AlarmManager
+
+        Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.SECOND, 10);
+       
+        PendingIntent pintent = PendingIntent.getService(this, 0, intent, 0);
+       
+        AlarmManager alarm = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        //for 30 mint 60*60*1000
+        alarm.setRepeating(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(),
+                     60*60*1000, pintent);
 	}
 
 	@Override
