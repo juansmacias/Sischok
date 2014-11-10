7ff14d6dd871ef19982a84bfa03f27358ebd3c7a
lainis20 <lainis20@gmail.com>
2014-11-10 14:55:52 -0500

    Create Pantallabloqueada.java
    clase que debe tener el método de la pantalla bloqueada que permita decir si un usuario salió de su estado de ebriedad.

---
+++ src/uniandes/sischok/Pantallabloqueada.java
@@ -0,0 +1,24 @@
+package uniandes.sischok;
+
+import android.app.ActionBar.LayoutParams;
+import android.app.Activity;
+import android.app.KeyguardManager;
+import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.PhoneStateListener;
+import android.telephony.TelephonyManager;
+import android.view.ViewGroup.MarginLayoutParams;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+/**
+ * Activity de la pantalla bloqueada
+ * @author LinaMariaDuarteSalinas
+ *
+ */
+public class Pantallabloqueada extends Activity
+{
+	
+
+}
